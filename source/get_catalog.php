659654fb6951c8daa3bf43912cdb4f11f5e5b0f4
XMypuK <xmypuk1985@mail.ru>
2023-12-20 03:38:36 +1000

    fix for PHP8.2

--- source/get_catalog.php
+++ source/get_catalog.php
@@ -9,100 +9,25 @@
 		$files = glob($type_folder . '/*.' . $type);
 		$metafile = $type_folder . '/meta.xml';
 
-		$meta_info_parser = new MetaInfoParser();
-
-		if ( file_exists($metafile) ) {
-			$meta_data = file_get_contents($metafile);
-			$meta_info_parser->parse($meta_data);
-		}
+		$sxMeta = simplexml_load_file($metafile);
 
 		foreach ( $files as $file ) {
 			$name = preg_replace('/.*[\\\\\\/]/', '', $file);
-			$file_info = null;
-
-			foreach ( $meta_info_parser->images as $image ) {
-				if ( $image['name'] == $name ) {
-					$file_info = $image;
-					break;
-				}
-			}
+			$sxImage = $sxMeta->xpath('/meta/image[name/text()="' . $name  . '"]');
+			$description = !is_null($sxImage) ? $sxImage[0]->xpath('description/text()')[0] : '';
+			$tags = !is_null($sxImage) ? array_map(
+			    fn($value): string => $value, 
+			    $sxImage[0]->xpath('tag/text()')
+			) : array();
 
 			$result[] = array(
 				'type' => $type,
 				'name' => $name,
-				'description' => $file_info ? $file_info['description'] : '',
-				'tags' => $file_info ? $file_info['tags'] : array()
+				'description' => $description,
+				'tags' => $tags
 			);
 		}
 	}
 
 	echo json_encode($result);
-
-	class MetaInfoParser {
-		var $parser;
-		var $images = array();
-		var $current_image = null;
-
-		var $depth = array();
-
-		function MetaInfoParser() {
-			$this->parser = xml_parser_create();
-
-			xml_set_object($this->parser, $this);
-			xml_parser_set_option($this->parser, XML_OPTION_CASE_FOLDING, 0);
-			xml_set_element_handler($this->parser, 'on_tag_open', 'on_tag_close');
-			xml_set_character_data_handler($this->parser, 'on_data');
-		}
-
-		function parse( $data ) {
-			xml_parse($this->parser, $data);
-		}
-
-		function clear() {
-			$this->images = array();
-		}
-
-		function cur_tag() {
-			if ( count($this->depth) > 0 ) {
-				return $this->depth[ count($this->depth) - 1 ];
-			}
-			else {
-				return null;
-			}
-		}
-
-		function on_tag_open( $parser, $name, $attrs ) {
-			$this->depth[] = $name;
-
-			if ( $name == 'image' && count($this->depth) == 2 ) {
-				$this->current_image = array(
-					'name' => '',
-					'description' => '',
-					'tags' => array()
-				);				
-			}
-		}
-
-		function on_tag_close( $parser, $name ) {
-			if ( $this->cur_tag() == $name ) {
-				if ( $name == 'image' && count($this->depth) == 2 ) {
-					$this->images[] = $this->current_image;
-					$this->current_image = null;
-				}
-
-				array_pop($this->depth);
-			}
-		}
-
-		function on_data( $parser, $data ) {
-			if ( $this->current_image && count($this->depth) == 3 ) {
-				switch ( $this->cur_tag() ) {
-					case 'name': $this->current_image['name'] .= $data; break;
-					case 'description': $this->current_image['description'] .= $data; break;
-					case 'tag': $this->current_image['tags'][] = $data; break;
-				}
-			}
-		}
-
-	}
 ?>
